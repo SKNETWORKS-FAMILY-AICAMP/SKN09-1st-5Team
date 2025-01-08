a3cfe7fc45dc9c0828822dafb8dbef4ed6faad4a
Leegwangwoon <leegwangwoon@naver.com>
2025-01-08 15:11:08 +0900

    end_streamlit_add_requir

--- pages/06_2023.py
+++ pages/06_2023.py
@@ -1,8 +1,8 @@
 import mysql.connector
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
-# MySQL 데이터베이스 연결
 conn = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -10,7 +10,6 @@ conn = mysql.connector.connect(
     database="ecardb"
 )
 
-# 연결이 제대로 되었는지 확인
 if conn.is_connected():
     st.write("MySQL 연결 성공!")
 else:
@@ -18,7 +17,6 @@ else:
 
 cursor = conn.cursor(dictionary=True)
 
-# 수정된 SQL 쿼리 작성 (electric_car_registration 테이블과 electric_car_subsidy 테이블을 JOIN)
 query = """
     SELECT e.id, s.name AS sido_name, s.division AS sido_division, e.year, e.subsidy, r.class, r.regists
     FROM electric_car_subsidy e
@@ -27,49 +25,53 @@ query = """
     WHERE e.year = '2023';
 """
 
-# 쿼리 실행
 cursor.execute(query)
 
-# 결과 가져오기
 data = cursor.fetchall()
 
-# 쿼리 실행 결과가 있는지 확인
 if len(data) == 0:
-    st.write("2023 전기차 보조금 데이터가 없습니다.")
+    st.write("2023년 전기차 보조금 데이터가 없습니다.")
 else:
-    # 데이터프레임으로 변환
     df = pd.DataFrame(data)
     
-    # 'id' 컬럼 삭제
     df = df.drop(columns=['id'])
     
-    # 인덱스 1부터 시작하도록 수정
     df.reset_index(drop=True, inplace=True)
     df.index += 1
 
-# 커서 종료
 cursor.close()
 conn.close()
 
-# 세션 상태로 버튼 클릭 여부를 관리
 if 'show_table' not in st.session_state:
     st.session_state.show_table = False
 
-# 버튼 클릭시, 테이블 표시 여부를 변경
-if st.button('2023 전기차 보조금 데이터 보기'):
-    st.session_state.show_table = True
+if st.button('2023년 전기차 보조금 데이터 보기'):
+    st.session_state.show_table = not st.session_state.show_table
 
-# 드롭다운을 사용하여 시도 선택
 if st.session_state.show_table:
-    # 시도 이름을 리스트로 가져오기
     unique_sido_names = df['sido_name'].unique()
     
-    # 시도 선택 드롭다운
     selected_sido = st.selectbox('시도 선택', unique_sido_names)
 
-    # 선택된 시도에 해당하는 데이터만 필터링
     filtered_df = df[df['sido_name'] == selected_sido]
     
-    # 필터링된 데이터 출력
-    st.title(f'{selected_sido} - 2023 전기차 보조금 및 출고대수 데이터')
+    st.title(f'{selected_sido} - 2023년 전기차 보조금 및 출고대수 데이터')
     st.dataframe(filtered_df)
+    
+    fig1 = px.scatter(filtered_df, 
+                      x='sido_division', 
+                      y=['subsidy', 'regists'],
+                      labels={'sido_division': 'Sido Division', 'value': 'Value'},
+                      title=f'{selected_sido} - Subsidy and Registrations per Sido Division',
+                      template="plotly_white")
+    fig1.update_traces(marker=dict(size=12, line=dict(width=2, color='DarkSlateGrey')), selector='scatter')
+    st.plotly_chart(fig1)
+
+    fig2 = px.scatter(df, 
+                      x='sido_division', 
+                      y=['subsidy', 'regists'],
+                      labels={'sido_division': 'Sido Division', 'value': 'Value'},
+                      title='Overall - Subsidy and Registrations per Sido Division',
+                      template="plotly_white")
+    fig2.update_traces(marker=dict(size=12, line=dict(width=2, color='DarkSlateGrey')), selector='scatter')
+    st.plotly_chart(fig2)
